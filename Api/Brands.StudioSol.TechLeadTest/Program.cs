407201490878c1300d29b805ae543ae3560b22c9
Gabriel <gabriel.silva.brandao7@gmail.com>
2022-09-15 08:42:11 -0300

    fix: :rocket: get port from environment

--- Api/Brands.StudioSol.TechLeadTest/Program.cs
+++ Api/Brands.StudioSol.TechLeadTest/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Brands.StudioSol.TechLeadTest
 {
     public class Program
     {
+        private const string PORT_ENVIRONMENT_VARIABLE = "PORT";
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -15,6 +18,7 @@ namespace Brands.StudioSol.TechLeadTest
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls($"https://*.:{Environment.GetEnvironmentVariable(PORT_ENVIRONMENT_VARIABLE)}");
                 });
     }
 }
